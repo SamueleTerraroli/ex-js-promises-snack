903b760d4b2ff38e926f865deb0433786029f19d
SamueleTerraroli <terraroli99@gmail.com>
2025-03-27 14:00:40 +0100

    add: exercise 1

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+Ottieni il titolo di un post con una Promise.
+
+Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
+🎯 Bonus: Ottieni l'intero post con l'autore
+Crea una funzione getPost(id) che recupera l'intero post. Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
+*/
+
+const getPostTitle = (id) => {
+    return new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(response => response.json())
+            .then(data => resolve(data.title))
+            .catch(error => reject(error));
+    });
+};
+getPostTitle(1)
+    .then(title => console.log(title))
+    .catch(error => console.log(error));
